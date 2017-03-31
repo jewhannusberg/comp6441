c43e7c40f9caab0b8b93ff5f57ac7fab81622cd9
Youhan Cheery <youhancheery@Youhans-MBP.gateway>
2017-03-31 20:26:07 +1100

    fixed challenge 2 and finished challenge 3

--- set1/fixed_xor.py
+++ set1/fixed_xor.py
@@ -12,26 +12,14 @@ XOR rules:
 1 xor 1 = 0
 """
 
-def hex_binary(hex_str):
-    x = bin(int(str(hex_str), 16))[2:]
-    bin_str = '0'*(8-len(x)%8) + x
-    return bin_str
+def strxor(a, b):     # xor two strings of different lengths
+    if len(a) > len(b):
+        return '%x' % (int(a[:len(b)],16)^int(b,16))
+    else:
+        return '%x' % (int(a,16)^int(b[:len(a)],16))
 
+# str1 = "1c0111001f010100061a024b53535009181c"
 
-def _xor(str1, str2):
-    out_str = int(str1, 2)^int(str2,2)
-    out_str = bin(out_str)[2:].zfill(len(str1))
-    return out_str
+# str2 = "686974207468652062756c6c277320657965"
 
-str1 = "1c0111001f010100061a024b53535009181c"
-
-str2 = "686974207468652062756c6c277320657965"
-
-bin_str1 = hex_binary(str1)
-
-bin_str2 = hex_binary(str2)
-
-xor_out = _xor(bin_str1, bin_str2)
-print xor_out
-hex_out = hex(int(xor_out, 2))
-print hex_out
+# print strxor(str1, str2)
--- set1/xor_cipher.py
+++ set1/xor_cipher.py
@@ -1,5 +1,29 @@
 """
 hex string has been XORd against a single character
 
-find the character and decrypt the message
+find the key and decrypt the message
 """
+
+import binascii
+
+def score_plaintext(s):
+    # get all alphabetic characters from each decoded string
+    letters = filter(lambda x: 'a'<=x<='z' or 'A'<=x<='Z', s) 
+    # return a value of how many alphabetic characters in each decoded string
+    return float(len(letters)) / len(s)
+
+xord_str = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+
+encoded = binascii.unhexlify(xord_str)
+
+res = []
+# loop through number of hex characters
+for xor_key in range(256):
+    # decode each byte
+    decoded = ''.join(chr(ord(x) ^ xor_key) for x in encoded)
+    # create a list of all decoded values
+    res.append([score_plaintext(decoded), ''.join(decoded)])
+
+# pick only first value of result list
+print max(res, key=lambda x: x[0])
+
