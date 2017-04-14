b1a774cecc1b144bf4238f1c9fd625dd798413f2
Youhan Cheery <youhancheery@Youhans-MBP.gateway>
2017-04-14 10:37:22 +1000

    finished challenge9--pkcs7 padding

---
+++ set2/challenge9.py
@@ -0,0 +1,29 @@
+'''
+Challenge 9: Implement PKCS#7 padding
+A block cipher transforms a fixed-sized block (usually 8 or 16 bytes) of plaintext into ciphertext.
+But we almost never want to transform a single block; we encrypt irregularly-sized messages.
+
+One way we account for irregularly-sized messages is by padding, creating a plaintext that is 
+an even multiple of the blocksize. The most popular padding scheme is called PKCS#7.
+
+So: pad any block to a specific block length, by appending the number of bytes of padding
+to the end of the block.
+'''
+class PKCS7Padder():
+
+    def __init__(self, block_size):
+        if block_size < 2 or block_size > 255:
+            raise PKCS7Encoder.InvalidBlockSizeError('The block size must be ' \
+                    'between 2 and 255, inclusive')
+        self.block_size = block_size
+
+    def pad(self, text):
+        text_length = len(text)
+        amount_to_pad = self.block_size - (text_length % self.block_size)
+        if amount_to_pad == 0:
+            amount_to_pad = self.block_size
+        pad = chr(amount_to_pad)
+        return text + pad * amount_to_pad
+
+padder = PKCS7Padder(block_size=20)
+print repr(padder.pad("YELLOW SUBMARINE"))
